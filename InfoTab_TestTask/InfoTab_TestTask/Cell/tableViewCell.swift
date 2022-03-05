1fb28c0527c6222949075ea622789b4ca655851a
Alexander <grigoryev_7x7@icloud.com>
2022-03-05 07:02:29 +0300

    The project is set, Interface Builder has been deleted

---
+++ InfoTab_TestTask/InfoTab_TestTask/Cell/tableViewCell.swift
@@ -0,0 +1,8 @@
+//
+//  tableViewCell.swift
+//  InfoTab_TestTask
+//
+//  Created by Alexander Grigoryev on 05.03.2022.
+//
+
+import Foundation
---
+++ InfoTab_TestTask/InfoTab_TestTask/Controller/TableViewController.swift
@@ -0,0 +1,19 @@
+//
+//  TableViewController.swift
+//  InfoTab_TestTask
+//
+//  Created by Alexander Grigoryev on 05.03.2022.
+//
+
+import UIKit
+
+class TableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
